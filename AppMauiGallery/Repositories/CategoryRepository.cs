5c59b5223218ece12878e251eb9e1c7222b9fbfc
César Vincícius de Paula Ferreira <cesarvinic7@hotmail.com>
2024-06-03 20:08:04 -0300

    Estudando estrutura de telas.

---
+++ AppMauiGallery/Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using MauiGallery.Models;
+using MauiGallery.Views.Layouts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiGallery.Repositories
+{
+    internal class CategoryRepository
+    {
+        public CategoryRepository() { }
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+            categories.Add(new Category
+            {
+                Name = "Layout",
+                Components = new List<Component> {
+                    new Component {
+                        Title = "StackLayout",
+                        Description="Organização sequencial dos elementos",
+                        Page = typeof(StackLayoutPage)
+
+                        //Page = new StackLayoutPage() //TODO - Performance - Evitar de instanciar páginas em lista. Aumenta muito a memória.
+                    },
+                    new Component
+                    { 
+                        Title = "Grid",
+                        Description="Organiza os elementos dentro de uma tabela.",
+                        Page = typeof(GridLayoutPage)
+                    }, 
+                    new Component 
+                    {
+                        Title = "AbsoluteLayout",
+                        Description="Libertade total para posicionar e dimensionar os elementos na tela.",
+                        Page = typeof(AbsoluteLayoutPage)
+                    }
+                }
+            });
+
+            return categories;
+        }
+    }
+}
---
+++ AppMauiGallery/Views/Menu.xaml.cs
@@ -0,0 +1,67 @@
+using MauiGallery.Repositories;
+
+namespace AppMauiGallery.Views;
+
+public partial class Menu : ContentPage
+{
+	public Menu()
+	{
+		InitializeComponent();
+
+		var categories = new CategoryRepository().GetCategories();
+
+		foreach (var item in categories)
+		{
+            var lblCategory = new Label();
+			lblCategory.Text = item.Name;
+			lblCategory.FontFamily = "OpenSansSemibold";
+
+			this.MenuContainer.Children.Add(lblCategory);
+
+			foreach (var component in item.Components)
+			{
+				var tap = new TapGestureRecognizer();
+				tap.CommandParameter = component.Page;
+				tap.Tapped += OnTapComponent_Tapped;
+
+				var lblComponentTitle = new Label();
+				lblComponentTitle.Text = component.Title;
+				lblComponentTitle.FontFamily = "OpenSansSemibold";
+				lblComponentTitle.Margin = new Thickness(20, 10, 0, 0);
+				lblComponentTitle.GestureRecognizers.Add(tap);
+
+				var lblComponentDescription = new Label();
+				lblComponentDescription.Text = component.Description;
+				lblComponentDescription.Margin = new Thickness(20, 0, 0, 0 );
+				lblComponentTitle.GestureRecognizers.Add(tap);
+
+                this.MenuContainer.Children.Add(lblComponentTitle);
+                this.MenuContainer.Children.Add(lblComponentDescription);
+            }
+        }
+    }
+
+	private void OnTapComponent_Tapped(object sender, EventArgs e)
+	{ 
+		var label = (Label)sender;
+		var tap = (TapGestureRecognizer)label.GestureRecognizers[0];
+        //var page = (Page)tap.CommandParameter;
+        var page = (Type)tap.CommandParameter;
+
+        //Faz a mudança de página.
+        //((FlyoutPage)App.Current.MainPage).Detail = page;
+		//Activator.CreateInstance - Resolve o problema de memória causado pela instância direta da página.
+        ((FlyoutPage)App.Current.MainPage).Detail = new NavigationPage((Page)Activator.CreateInstance(page));
+
+        //Recolhe o menu
+        ((FlyoutPage)App.Current.MainPage).IsPresented = false;
+
+    }
+
+    private void OnTapInicio_Tapped(object sender, TappedEventArgs e)
+    {
+		
+        ((FlyoutPage)App.Current.MainPage).Detail = new NavigationPage(new AppMauiGallery.Views.MainPage());
+        ((FlyoutPage)App.Current.MainPage).IsPresented = false;
+    }
+}
