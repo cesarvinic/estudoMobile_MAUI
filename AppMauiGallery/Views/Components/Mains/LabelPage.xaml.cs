808727ee27eb740c9797837ad374d4a57102c437
César Vincícius de Paula Ferreira <cesarvinic7@hotmail.com>
2024-06-04 20:12:23 -0300

    Alinhamento de texto independnete do componente.

--- AppMauiGallery/Views/Components/Mains/LabelPage.xaml.cs
+++ AppMauiGallery/Views/Components/Mains/LabelPage.xaml.cs
@@ -1,9 +1,26 @@
+using System.Windows.Input;
+
 namespace MauiGallery.Views.Components.Mains;
 
 public partial class LabelPage : ContentPage
 {
+    public ICommand clique {  get; private set; }
+
 	public LabelPage()
 	{
 		InitializeComponent();
+        clique = new Command(OnCommandTapped);
 	}
+
+    private void OnSpanTapped(object sender, TappedEventArgs e)
+    {
+        // Ação a ser realizada quando o Span for clicado
+        DisplayAlert("Span Clicked", "Você clicou no texto sublinhado!", "OK");
+    }
+
+    private void OnCommandTapped()
+    {
+        // Ação a ser realizada quando o Span for clicado
+        DisplayAlert("Span Clicked", "Você clicou no texto sublinhado!", "OK");
+    }
 }
