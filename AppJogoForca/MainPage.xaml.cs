7fb04a2a9216e111dffe90b173b8aeb1712f9320
César Vincícius de Paula Ferreira <cesarvinic7@hotmail.com>
2024-06-06 22:10:28 -0300

    Inclusão do método para identificar as palavras. Inclusão das imagens. Codificação da mensagem de alerta após tentativas e erros.

--- AppJogoForca/MainPage.xaml.cs
+++ AppJogoForca/MainPage.xaml.cs
@@ -1,25 +1,104 @@
-﻿namespace AppJogoForca
+﻿using AppJogoForca.Libraries.Text;
+using AppJogoForca.Models;
+using AppJogoForca.Repositories;
+using System.Runtime.CompilerServices;
+
+namespace AppJogoForca
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        private Word _word;
+        private int _errors;
         public MainPage()
         {
             InitializeComponent();
-        }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+            //Inicia uma nova partida.
+            ResetScreen();
+        }
+        private async void OnButtonClicked(object sender, EventArgs e)
         {
-            count++;
+            //Desabilita o botão clicado. 
+            ((Button)sender).IsEnabled = false;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            //Obtém a letra do botão clicado
+            string letter = ((Button)sender).Text;
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            //Verifica se a letra existe na palavra e obtém a posição da letra.
+            var positions = _word.Text.GetPositions(letter);
+
+            //Se não encontrou a letra.
+            if (positions.Count == 0)
+            {
+                _errors++;
+
+                //Altera a imagem mediante o erro.
+                this.ImgForca.Source = ImageSource.FromFile($"forca{_errors + 1}.png");
+
+                if (_errors == 6)
+                {
+                    await DisplayAlert("Perdeu!", "Você foi enforcado!", "Novo jogo");
+
+                    //Inicia uma nova partida.
+                    ResetScreen();
+                }
+
+                return;
+            }
+
+            foreach (int position in positions)
+            {
+                //Se encontrar a letra, remove o caractere '_' e insere a letra na posição correta.
+                this.lblText.Text = lblText.Text.Remove(position, 1).Insert(position, letter);
+            }
         }
-    }
 
+        private void ResetScreen()
+        {
+            //Reinicia o teclado
+            ResetVirtualKeyboard();
+
+            //Reinicia os erros
+            ResetErrors();
+
+            //Sorteia uma nova palavra.
+            SortNewWord();
+        }
+
+        private void ResetErrors()
+        {
+            //Contador de erros.
+            _errors = 0;
+
+            //Imagem inicial.
+            this.ImgForca.Source = ImageSource.FromFile($"forca1.png");
+        }
+
+        private void ResetVirtualKeyboard()
+        {
+            //Habilta todos os botões da tela. 
+            ResetVirtualLines((HorizontalStackLayout)this.KeyboardContainer.Children[0]);
+            ResetVirtualLines((HorizontalStackLayout)this.KeyboardContainer.Children[1]);
+            ResetVirtualLines((HorizontalStackLayout)this.KeyboardContainer.Children[2]);
+        }
+
+        private void ResetVirtualLines(HorizontalStackLayout horizontal)
+        {
+            foreach (Button button in horizontal.Children)
+            {
+                button.IsEnabled = true;
+            }
+        }
+
+        private void SortNewWord()
+        {
+            //Seleciona uma palavra aleatoreamente.
+            var repository = new WordRepositories();
+            _word = repository.GetRandomWord();
+
+            this.lblTips.Text = _word.Tips;
+            this.lblText.Text = new string('_', _word.Text.Length);
+        }
+
+    }
 }
---
+++ AppJogoForca/Repositories/WordRepositories.cs
@@ -0,0 +1,151 @@
+﻿using AppJogoForca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.Maui.ApplicationModel.Permissions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AppJogoForca.Repositories
+{
+    public class WordRepositories
+    {
+        private List<Word> _words;
+
+        public WordRepositories() { 
+            _words =
+            [
+                new Word("Nome", "Maria".ToUpper()),
+                new Word("Animal", "Elefante".ToUpper().ToUpper()),
+                new Word("Animal", "Leão".ToUpper()),
+                new Word("Animal", "Tigre".ToUpper()),
+                new Word("Animal", "Girafa".ToUpper()),
+                new Word("Animal", "Zebra".ToUpper()),
+                new Word("Animal", "Canguru".ToUpper()),
+                new Word("Animal", "Urso".ToUpper()),
+                new Word("Animal", "Coelho".ToUpper()),
+                new Word("Animal", "Rato".ToUpper()),
+                new Word("Animal", "Macaco".ToUpper()),
+                new Word("Animal", "Gorila".ToUpper()),
+                new Word("Animal", "Hipopótamo".ToUpper()),
+                new Word("Animal", "Rinoceronte".ToUpper()),
+                new Word("Animal", "Crocodilo".ToUpper()),
+                new Word("Animal", "Flamingo".ToUpper()),
+                new Word("Animal", "Pinguim".ToUpper()),
+                new Word("Animal", "Golfinho".ToUpper()),
+                new Word("Animal", "Tubarão".ToUpper()),
+                new Word("Animal", "Baleia".ToUpper()),
+                new Word("Animal", "Foca".ToUpper()),
+                new Word("Fruta", "Abacaxi".ToUpper()),
+                new Word("Fruta", "Acerola".ToUpper()),
+                new Word("Fruta", "Amora".ToUpper()),
+                new Word("Fruta", "Banana".ToUpper()),
+                new Word("Fruta", "Caju".ToUpper()),
+                new Word("Fruta", "Cereja".ToUpper()),
+                new Word("Fruta", "Coco".ToUpper()),
+                new Word("Fruta", "Figo".ToUpper()),
+                new Word("Fruta", "Goiaba".ToUpper()),
+                new Word("Fruta", "Jaca".ToUpper()),
+                new Word("Fruta", "Kiwi".ToUpper()),
+                new Word("Fruta", "Laranja".ToUpper()),
+                new Word("Fruta", "Limão".ToUpper()),
+                new Word("Fruta", "Maçã".ToUpper()),
+                new Word("Fruta", "Mamão".ToUpper()),
+                new Word("Fruta", "Manga".ToUpper()),
+                new Word("Fruta", "Maracujá".ToUpper()),
+                new Word("Fruta", "Melancia".ToUpper()),
+                new Word("Fruta", "Morango".ToUpper()),
+                new Word("Fruta", "Pêssego".ToUpper()),
+                new Word("País", "Brasil".ToUpper()),
+                new Word("País", "Argentina".ToUpper()),
+                new Word("País", "Canadá".ToUpper()),
+                new Word("País", "Estados Unidos".ToUpper()),
+                new Word("País", "México".ToUpper()),
+                new Word("País", "França".ToUpper()),
+                new Word("País", "Alemanha".ToUpper()),
+                new Word("País", "Itália".ToUpper()),
+                new Word("País", "Espanha".ToUpper()),
+                new Word("País", "Japão".ToUpper()),
+                new Word("País", "China".ToUpper()),
+                new Word("País", "Índia".ToUpper()),
+                new Word("País", "Austrália".ToUpper()),
+                new Word("País", "Rússia".ToUpper()),
+                new Word("País", "África do Sul".ToUpper()),
+                new Word("País", "Egito".ToUpper()),
+                new Word("País", "Noruega".ToUpper()),
+                new Word("País", "Suécia".ToUpper()),
+                new Word("País", "Finlândia".ToUpper()),
+                new Word("País", "Portugal".ToUpper()),
+                new Word("Profissão", "Médico".ToUpper()),
+                new Word("Profissão", "Engenheiro".ToUpper()),
+                new Word("Profissão", "Professor".ToUpper()),
+                new Word("Profissão", "Advogado".ToUpper()),
+                new Word("Profissão", "Enfermeiro".ToUpper()),
+                new Word("Profissão", "Arquiteto".ToUpper()),
+                new Word("Profissão", "Jornalista".ToUpper()),
+                new Word("Profissão", "Contador".ToUpper()),
+                new Word("Profissão", "Psicólogo".ToUpper()),
+                new Word("Profissão", "Dentista".ToUpper()),
+                new Word("Profissão", "Programador".ToUpper()),
+                new Word("Profissão", "Farmacêutico".ToUpper()),
+                new Word("Profissão", "Cozinheiro".ToUpper()),
+                new Word("Profissão", "Piloto".ToUpper()),
+                new Word("Profissão", "Veterinário".ToUpper()),
+                new Word("Profissão", "Fisioterapeuta".ToUpper()),
+                new Word("Profissão", "Eletricista".ToUpper()),
+                new Word("Profissão", "Bombeiro".ToUpper()),
+                new Word("Profissão", "Mecânico".ToUpper()),
+                new Word("Profissão", "Designer".ToUpper()),
+                new Word("Nome pessoa", "Ana".ToUpper()),
+                new Word("Nome pessoa", "João".ToUpper()),
+                new Word("Nome pessoa", "Pedro".ToUpper()),
+                new Word("Nome pessoa", "Maria".ToUpper()),
+                new Word("Nome pessoa", "Lucas".ToUpper()),
+                new Word("Nome pessoa", "Laura".ToUpper()),
+                new Word("Nome pessoa", "Gabriel".ToUpper()),
+                new Word("Nome pessoa", "Juliana".ToUpper()),
+                new Word("Nome pessoa", "Matheus".ToUpper()),
+                new Word("Nome pessoa", "Sofia".ToUpper()),
+                new Word("Nome pessoa", "Rafael".ToUpper()),
+                new Word("Nome pessoa", "Beatriz".ToUpper()),
+                new Word("Nome pessoa", "Felipe".ToUpper()),
+                new Word("Nome pessoa", "Carolina".ToUpper()),
+                new Word("Nome pessoa", "Gustavo".ToUpper()),
+                new Word("Nome pessoa", "Amanda".ToUpper()),
+                new Word("Nome pessoa", "Leonardo".ToUpper()),
+                new Word("Nome pessoa", "Mariana".ToUpper()),
+                new Word("Nome pessoa", "Bruno".ToUpper()),
+                new Word("Nome pessoa", "Fernanda".ToUpper()),
+                new Word("Vegetal", "Alface".ToUpper()),
+                new Word("Vegetal", "Espinafre".ToUpper()),
+                new Word("Vegetal", "Couve".ToUpper()),
+                new Word("Vegetal", "Brócolis".ToUpper()),
+                new Word("Vegetal", "Cenoura".ToUpper()),
+                new Word("Vegetal", "Beterraba".ToUpper()),
+                new Word("Vegetal", "Abobrinha".ToUpper()),
+                new Word("Vegetal", "Berinjela".ToUpper()),
+                new Word("Vegetal", "Pimentão".ToUpper()),
+                new Word("Vegetal", "Pepino".ToUpper()),
+                new Word("Vegetal", "Rúcula".ToUpper()),
+                new Word("Vegetal", "Agrião".ToUpper()),
+                new Word("Vegetal", "Tomilho".ToUpper()),
+                new Word("Vegetal", "Nabo".ToUpper()),
+                new Word("Vegetal", "Rabanete".ToUpper()),
+                new Word("Vegetal", "Ervilha".ToUpper()),
+                new Word("Vegetal", "Vagem".ToUpper()),
+                new Word("Vegetal", "Aipo".ToUpper()),
+                new Word("Vegetal", "Tomate".ToUpper()),
+                new Word("Vegetal", "Cebola".ToUpper()),
+            ];
+        }
+
+        public Word GetRandomWord()
+        {
+            Random rand = new Random();
+            var number = rand.Next(0, _words.Count);
+            return _words[number];
+        }
+    }
+}
